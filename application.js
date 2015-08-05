05d9b4130b3dadae5b798b8bccd732abaa0d866e
mercedesquddoos <mjquddoos@gmail.com>
2015-08-04 19:29:42 -0500

    modulus operators

---
+++ application.js
@@ -0,0 +1,12 @@
+for (var i = 1; i <= 1000; i++) {
+    if(i % 2 == 0){ 
+        document.write("<span style='color:red;'>"+i+"</span><br />");
+    } else {
+        
+    }
+  if(i % 23 == 0){ 
+        document.write("<span style='color:red;'>"+i+"</span><br />");
+    } else {
+        
+    }
+}
